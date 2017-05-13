4e94a2906e8825268791c7adf37517721432df41
babymustgrowstrong <babymustgrowstrong@yahoo.com>
2017-05-13 15:03:44 -0700

    GL_EXT_draw_buffers_indexed support

--- renderdoc/driver/gl/gl_renderstate.cpp
+++ renderdoc/driver/gl/gl_renderstate.cpp
@@ -1484,12 +1484,9 @@ void GLRenderState::ApplyState(void *ctx, WrappedOpenGL *gl)
 
   m_Real->glClearStencil((GLint)StencilClearValue);
 
-  if(m_Real->glColorMaski)
-  {
 	for (GLuint i = 0; i < RDCMIN(maxDraws, (GLuint)ARRAY_COUNT(ColorMasks)); i++)
 	  m_Real->glColorMaski(i, ColorMasks[i].red, ColorMasks[i].green, ColorMasks[i].blue,
 	    ColorMasks[i].alpha);
-  }
 
   m_Real->glSampleMaski(0, (GLbitfield)SampleMask[0]);
   m_Real->glSampleCoverage(SampleCoverage, SampleCoverageInvert ? GL_TRUE : GL_FALSE);
